05c61b276ffa68f6d18f16d305c05ec50593843c
naos0327 <sebastianbarandicao@gmail.com>
2024-08-14 11:09:35 -0500

    metodos
    agregue los metodos del taller

---
+++ taller1.js
@@ -0,0 +1,30 @@
+function convertidorTemp(C) {
+  let F = (C * 9) / 5 + 32
+  return "el valor de " + C + "C° en F es " + F
+}
+
+function resolvedor(a,b,c,signo) {
+    let op = 1
+    if (signo)
+        op = -1
+    return ( - b + op *  ( b ** 2 - 4 * a * c) ** (1/2)) / ( 2 * a) 
+}
+
+function mejorParidad(x){
+    if (x/2===parseInt(x/2)){
+        return "par"
+    }else{
+        return "inpar"
+    }
+}
+
+function peorParidad(x, t =true){
+
+    if(x>0){
+       t = peorParidad(x-1,!t)
+       return t
+    }
+    if(t)
+        return "par"
+    return "inpar"
+}
